3fcc10a1323a84ab7dbcdb102cb6e9e3d12da146
arkhurst <fiiarkhurst@gmail.com>
2022-03-06 11:54:25 +0000

    added css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="traffic-light-container">
+        <input type="radio" readOnly className="light red" />
+        <input type="radio" readOnly className="light yellow" />
+        <input type="radio" readOnly className="light green" />
+      </div>
     </div>
   );
 }
